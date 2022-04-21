df6837c236df0a7e06de06b8ff40573a4f0ef069
Aydogan Beyza <bnur933@gmail.com>
2022-04-21 16:04:29 +0300

    DOING FINAL WORK ON HELLO FEATURE

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+        System.out.println("EU8 LEARNING GIT AND GITHUB");
 
     }
 
